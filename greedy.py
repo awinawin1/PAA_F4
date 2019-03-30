31e5abc7ac94ab26dda15c3a2b39eeeb15f6cb40
awinawin1 <43516997+awinawin1@users.noreply.github.com>
2019-03-30 16:51:08 +0700

    Update greedy.py
    update

--- greedy.py
+++ greedy.py
@@ -8,23 +8,22 @@ def Greedy(map, start, goal):
 	visited = set()	#mark visited city
 	parent = {}		#mark the source to reach a city
 	gn = {start : 0}	
-	fn = {start : 0} 
 
 	pq = []#list pq
 
-	heapq.heappush(pq, (fn[start], start)) #enter to priorityqueue
+	heapq.heappush(pq, (gn[start], start)) #enter to priorityqueue
 	cnt = 1 #intialitation for order of step
 	while pq:
 		current = heapq.heappop(pq)[1] #push city to priorityque
 		print()
 		print('Step -{}'.format(cnt))
 		cnt+=1
-		print('Now at {}'.format(current)) #print current city
+		print('Sekarang berada di kota {}'.format(current)) #print current city
 
 		if current == goal: #ketika lokasi = tujuan langsung print Reach Goal
 			print('Reach GOAL')
 			print()
-			path = []#menyediakan path 
+			path = []#Menyimpan Jalur fix
 			while current in parent: 
 				path.append(current)#mengepush current ke path
 				current = parent[current]
@@ -35,17 +34,15 @@ def Greedy(map, start, goal):
 			if c_name in visited:			#city had been visited before
 				continue
             
-			gn_temp =  map[current][c_name]  	
-			fn_temp = gn_temp 			
-			print('Sekarang Cek at {:5}\tg(n) = {} Kilo\t'.format(c_name, gn_temp))
-			if c_name in fn and fn_temp < fn[c_name] or c_name not in fn:	# ketika f(n) lebih kecil dari f(n) yng 
+			gn_temp =  map[current][c_name]
+			print('kota {:5}\tg(n) = {} Kilo\t'.format(c_name, gn_temp))
+			if c_name in gn and gn_temp < gn[c_name] or c_name not in gn:	# ketika f(n) lebih kecil dari f(n) yng 
                                                                         #pernaah di savenew  
                                                                         #atau kota yang belum pernah
-				if c_name not in fn:											#expand to newly discovered city
-					heapq.heappush(pq, (fn_temp, c_name)) #when push ti priorytyque
-					print('Push {} to heap'.format(c_name))
+				if c_name not in gn:											#expand to newly discovered city
+					heapq.heappush(pq, (gn_temp, c_name)) #when push ti priorytyque
+					print('Masukkan {} Ke Priorty Queue'.format(c_name))
 				parent[c_name] = current #set source to reach city
 				gn[c_name] = gn_temp		#update or set g(n)
-				fn[c_name] = fn_temp		#update or set f(n)
 
-#return None
+	return None
